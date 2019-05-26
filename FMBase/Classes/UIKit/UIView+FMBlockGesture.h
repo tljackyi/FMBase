1a56c9363f94ce270d41a5cbf9297e3104668140
yitailong <tljackyi@gmail.com>
2019-05-26 09:00:24 +0800

    增加BlockGesture

---
+++ FMBase/Classes/UIKit/UIView+FMBlockGesture.h
@@ -0,0 +1,31 @@
+//
+//  UIView+FMBlockGesture.h
+//  Pods
+//
+//  Created by yitailong on 2019/5/26.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef void(^FMTapGestureBlock)(UITapGestureRecognizer *tapGestureRecognizer);
+typedef void(^FMLongPressGestureBlock)(UILongPressGestureRecognizer *lonePressGestureRecognizer);
+
+@interface UIView (FMBlockGesture)
+
+/**
+ Add tap gesture with block.
+ @param handler block handler
+ */
+- (void)fm_addTapGestureWithHandler:(FMTapGestureBlock)handler;
+
+/**
+ Add long press gusture with block.
+ @param handler block handler
+ */
+- (void)fm_addLongPressGestureWithHandler:(FMLongPressGestureBlock)handler;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FMBase/Classes/UIKit/UIView+FMBlockGesture.m
@@ -0,0 +1,57 @@
+//
+//  UIView+FMBlockGesture.m
+//  Pods
+//
+//  Created by yitailong on 2019/5/26.
+//
+
+#import "UIView+FMBlockGesture.h"
+#import <objc/runtime.h>
+
+static const void *kFMTapGestureKey = &kFMTapGestureKey;
+static const void *kFMTapGestureBlockKey = &kFMTapGestureBlockKey;
+static const void *kFMLongPressGestureKey = &kFMLongPressGestureKey;
+static const void *kFMLongPressBlockKey = &kFMLongPressBlockKey;
+
+@implementation UIView (FMBlockGesture)
+
+- (void)fm_addTapGestureWithHandler:(FMTapGestureBlock)handler {
+    UITapGestureRecognizer *tapGesture =objc_getAssociatedObject(self, kFMTapGestureKey);
+    if (!tapGesture) {
+        tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(fm_tapAcion:)];
+        [self addGestureRecognizer:tapGesture];
+        objc_setAssociatedObject(self, kFMTapGestureKey, tapGesture, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    objc_setAssociatedObject(self, kFMTapGestureBlockKey, handler, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (void)fm_addLongPressGestureWithHandler:(FMLongPressGestureBlock)handler {
+    UILongPressGestureRecognizer *longPressGesture = objc_getAssociatedObject(self, kFMLongPressGestureKey);
+    if (!longPressGesture) {
+        longPressGesture = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(longPressAction:)];
+        [self addGestureRecognizer:longPressGesture];
+        objc_setAssociatedObject(self, kFMLongPressGestureKey, longPressGesture, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    objc_setAssociatedObject(self, kFMLongPressBlockKey, handler, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+#pragma mark ------ < Event Response > ------
+- (void)fm_tapAcion:(UITapGestureRecognizer *)tapGestureRecognizer {
+    if (tapGestureRecognizer.state == UIGestureRecognizerStateRecognized) {
+        FMTapGestureBlock block = objc_getAssociatedObject(self, kFMTapGestureBlockKey);
+        if (block) {
+            block(tapGestureRecognizer);
+        }
+    }
+}
+
+- (void)longPressAction:(UILongPressGestureRecognizer *)longPressGestureRecognizer {
+    if (longPressGestureRecognizer.state == UIGestureRecognizerStateRecognized) {
+        FMLongPressGestureBlock block = objc_getAssociatedObject(self, kFMLongPressBlockKey);
+        if (block) {
+            block(longPressGestureRecognizer);
+        }
+    }
+}
+
+@end
